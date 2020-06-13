2867e799cac82f1c72e4e6ef0b6c650745a9a506
Felipe Dacal Fragoso <fefragoso@gmail.com>
2020-06-13 17:04:13 +0200

    add the week days name on header

--- src/components/calendar/index.js
+++ src/components/calendar/index.js
@@ -1,10 +1,26 @@
 import React from "react";
+import moment from 'moment'
 
 export default class Calendar extends React.Component {
+  weekdayshort = moment.weekdaysShort();
+  
   render() {
+    let weekdayshortname = this.weekdayshort.map(day => {
+        return (
+          <th key={day} className="week-day">
+           {day}
+          </th>
+        );
+     });
+     
     return (
       <div>
         <h2>Calendar</h2>
+        <table className="calendar-day">
+          <thead>
+            <tr>{weekdayshortname}</tr>
+          </thead>
+        </table>
       </div>
     );
   }
