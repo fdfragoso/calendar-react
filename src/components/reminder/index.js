eeffa33aaec67a2e1a75d5b2b7e66b7d8aceeafa
Felipe Dacal Fragoso <fefragoso@gmail.com>
2020-06-14 15:28:51 +0200

    create reminder component

---
+++ src/components/reminder/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './reminder.css';
+
+export default class Reminder extends React.Component {
+  render() {
+    return (
+      <h1>Hello World</h1>
+    );
+  }
+}
