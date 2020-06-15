3f5792627289a46a166bad93f30d4ce0fb398c79
Felipe Dacal Fragoso <fefragoso@gmail.com>
2020-06-15 20:14:21 +0200

    add redux and refactor more code

--- src/index.js
+++ src/index.js
@@ -1,19 +1,33 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+import registerServiceWorker from "./registerServiceWorker";
+import "./styles/main.css";
 
-import Calendar from "./components/calendar";
+// Redux
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
 
-function App() {
-  return (
-    <div className="App">
-      <Calendar />
-    </div>
-  );
-}
+// Reducers
+import reminders from "./store/reducers/reminders";
 
-const rootElement = document.getElementById("root");
+// Redux Chrome Devtool Extension
+const composeEnhancers =
+  process.env.NODE_ENV === "development"
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    : null || compose;
 
-ReactDOM.render(
-  <App />, 
-  rootElement
+const store = createStore(reminders, composeEnhancers(applyMiddleware(thunk)));
+
+const appShell = (
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
 );
+
+ReactDOM.render(appShell, document.getElementById("root"));
+registerServiceWorker();
