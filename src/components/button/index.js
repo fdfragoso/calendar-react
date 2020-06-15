8f3ca09ae905905bf347009e602ef58793571677
Felipe Dacal Fragoso <fefragoso@gmail.com>
2020-06-15 14:24:03 +0200

    add modal with all input fields

--- src/components/button/index.js
+++ src/components/button/index.js
@@ -1,10 +1,15 @@
 import React from "react";
+
 import Modal from "react-bootstrap/Modal";
 import ModalBody from "react-bootstrap/ModalBody";
 import ModalHeader from "react-bootstrap/ModalHeader";
 import ModalFooter from "react-bootstrap/ModalFooter";
 import ModalTitle from "react-bootstrap/ModalTitle";
-import moment from "moment";
+
+import Weather from '../weather/index.js';
+
+import { faClock, faCalendarAlt } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 
@@ -12,9 +17,15 @@ export default class Button extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      isOpen: false
+      isOpen: false,
+      date: new Date()
     }
   }
+
+  handleChange = date => {
+    this.setState({date: date});
+    console.log(this.state.date);
+  };
   
   showModal = () => {
     console.log(this.props.dateObject.format("DD-MM-YYYY"));
@@ -36,14 +47,28 @@ export default class Button extends React.Component {
         <Modal show={this.state.isOpen} onHide={this.hideModal}>
           <ModalHeader>
             <ModalTitle>
+              <input 
+                type="text"
+                placeholder="Add the reminder"
+              />
+            </ModalTitle>
+            <button onClick={this.hideModal}>X</button>
+          </ModalHeader>
+          <ModalBody>
+            <FontAwesomeIcon icon={faCalendarAlt} />
             <input 
               type="text"
               placeholder={this.props.dia}
             />
-            </ModalTitle>
-            <button onClick={this.hideModal}>X</button>
-          </ModalHeader>
-          <ModalBody>The body</ModalBody>
+            <br/>
+            <FontAwesomeIcon icon={faClock} />
+            <input 
+              type="text"
+              placeholder="00:00"
+            />
+            <br/>
+            <Weather />
+          </ModalBody>
           <ModalFooter>
             <button onClick={this.hideModal}>Cancel</button>
             <button>Save</button>
