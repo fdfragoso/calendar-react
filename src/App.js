97c684f9b8e3a52f0ffba3b4b0a875af9068660e
Felipe Dacal Fragoso <fefragoso@gmail.com>
2020-06-19 01:26:36 +0200

    fix another folder with the same issue

--- src/App.js
+++ src/App.js
@@ -1,21 +1,37 @@
 import React from "react";
-import { Route, Switch, HashRouter } from "react-router-dom";
+import {
+  Route,
+  Switch,
+  HashRouter
+} from "react-router-dom";
 
-import Calendar from "./hoc/Calendar/index.js";
+import Calendar from "./hoc/calendar/index.js";
 import Month from "./containers/month/index.js";
 
 export default class App extends React.Component {
   render() {
     return (
-      
-      <Calendar>
-        <HashRouter>
-          <Switch>
-            <Route path="/:year/:month" component={Month} />
-            <Route path="/" exact component={Month} />
-          </Switch>
-        </HashRouter>
-      </Calendar>
+
+      <
+      Calendar >
+      <
+      HashRouter >
+      <
+      Switch >
+      <
+      Route path = "/:year/:month"
+      component = {
+        Month
+      }
+      /> <
+      Route path = "/"
+      exact component = {
+        Month
+      }
+      /> <
+      /Switch> <
+      /HashRouter> <
+      /Calendar>
     );
   }
 }
