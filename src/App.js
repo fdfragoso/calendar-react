df0b0c6dc8136d702a22297266404b64d1bf3da5
Felipe Dacal Fragoso <fefragoso@gmail.com>
2020-06-19 01:20:29 +0200

    fixed the folder name - on git is duplicated for some reason

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Route, Switch, HashRouter } from "react-router-dom";
 
 import Calendar from "./hoc/Calendar/index.js";
-import Month from "./containers/Month/index.js";
+import Month from "./containers/month/index.js";
 
 export default class App extends React.Component {
   render() {
